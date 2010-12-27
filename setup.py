e1a5b469463395dacbf0116d1bc4fcc963beaee0
Josh VanderLinden <codekoala@gmail.com>
2010-12-27 12:04:12 -0500

    Changed the email address in the package info

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='django-queued-storage',
     version='0.2',
     description='Provides a proxy for django file storage, that allows you to upload files locally and eventually serve them remotely',
-    author='Sean Brant',
-    author_email='brant.sean@gmail.com',
+    author='Sean Brant, Josh VanderLinden',
+    author_email='codekoala@gmail.com',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python',
