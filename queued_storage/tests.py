50bfd85c3247c5e067c87acf6d929e7346bef954
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2010-12-29 08:59:06 -0600

    Started to add tests (unfinished work).

--- queued_storage/tests.py
+++ queued_storage/tests.py
@@ -1,23 +1,48 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
+For simplicity — and to avoid requiring a paid-for account on some cloud
+storage system — testing is conducted against two local storage backends. Since
+the QueuedRemoteStorage backend is truly agnostic about the local and remote
+storage systems, this should work as transparently as using one (or even two!)
+remote storage systems.
 """
 
-from django.test import TestCase
+import shutil
+import tempfile
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from django.tests import TestCase
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+from queued_storage import backend
 
->>> 1 + 1 == 2
-True
-"""}
+
+class BasicTest(TestCase):
+    """
+    Common setUp and tearDown methods for storage testing. "Stuff" is stored in
+    a temporary directory and cleaned up afterwards.
+    """
+
+    def setUp(self):
+        self.local_dir = tempfile.mkdtemp()
+        self.remote_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.local_dir)
+        shutil.rmtree(self.remote_dir)
+
+
+class StorageTests(BasicTest):
+    def test_storage_saves_locally_and_remotely(self):
+        klass = "django.core.files.storage.FileSystemStorage"
+        storage = backend.QueuedRemoteStorage(local=klass, remote=klass,
+                local_kwargs={"location": self.local_dir},
+                remote_kwargs={"location": self.remote_dir})
+
+    # TODO:
+    #   - simple saving works
+    #   - save without running celery task still has file available locally.
+    #   - save clearing cache key checks if file is available remotely and uses
+    #   that if possible.
+
+
+# class FieldTests(BasicTest):
+    # TODO: Test the storage as part of a field class.
 
