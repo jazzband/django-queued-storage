69dcc4a2ac275ba4dda775b6f0674237b56bcd4a
Jannis Leidel <jannis@leidel.info>
2012-01-05 23:17:31 +0100

    Forgot django-jenkins settings.

--- queued_storage/test_settings.py
+++ queued_storage/test_settings.py
@@ -21,3 +21,10 @@ BROKER_TRANSPORT = "memory"
 CELERY_IGNORE_RESULT = True
 CELERYD_LOG_LEVEL = "DEBUG"
 CELERY_DEFAULT_QUEUE = "queued_storage"
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',
+)
