87a177aa285062ce0b7f8bd230cb13bb43ff768a
Jannis Leidel <jannis@leidel.info>
2013-04-10 17:41:56 +0200

    Use standalone celery instead of django-celery since the latter isn't on py3k yet.

--- queued_storage/test_settings.py
+++ queued_storage/test_settings.py
@@ -8,18 +8,9 @@ DATABASES = {
 }
 
 INSTALLED_APPS = [
-    'djcelery',
     'queued_storage',
     'queued_storage.tests',
 ]
-
-import djcelery
-djcelery.setup_loader()
-
-BROKER_TRANSPORT = "memory"
-CELERY_IGNORE_RESULT = True
-CELERYD_LOG_LEVEL = "DEBUG"
-CELERY_DEFAULT_QUEUE = "queued_storage"
 
 TEST_RUNNER = 'discover_runner.DiscoverRunner'
 
