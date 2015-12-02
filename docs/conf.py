fce670192b842f9dbb4d3166a7c5e13c555c5a81
Jannis Leidel <jannis@leidel.info>
2015-12-02 10:46:09 +0100

    Fix autodoc.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'queued_storage.test_settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 # -- General configuration -----------------------------------------------------
 
